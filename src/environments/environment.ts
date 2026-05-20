a58a7be259c1dffb09a0c6bca387df0618511af2
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-05-20 10:54:21 +0200

    primer commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  //apiUrl: 'http://localhost:3000/api',
+  //URL_IMGS:'http://localhost:3000/imgsUsers/',
+  //URL_IMGS_ARTICLE:'http://localhost:3000/imgsArticles/'
+};
