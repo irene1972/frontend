268eb9cf67f46d7d8343a4bc6d97126a9c004601
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-06-09 08:47:54 +0200

    maquetación del login

--- src/app/pages/login-component/login-component.component.ts
+++ src/app/pages/login-component/login-component.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { LoginRegister } from '../../shared/login-register/login-register';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-component',
-  imports: [],
+  imports: [LoginRegister,ReactiveFormsModule,RouterLink],
   templateUrl: './login-component.component.html',
   styleUrl: './login-component.component.css',
 })
 export class LoginComponentComponent {
+miForm: FormGroup;
+  mensaje: string = '';
+  tipo: boolean = false;
 
+   constructor(private cd: ChangeDetectorRef, private router: Router, private route: ActivatedRoute) {
+    this.miForm = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8)
+      ])
+    }, []);
+  }
+
+   get email() {
+    return this.miForm.get('email');
+  }
+
+  get password() {
+    return this.miForm.get('password');
+  }
+
+ cargarDatos() {
+    if (!this.miForm.valid) {
+      this.miForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.miForm.value);
+  }
 }
