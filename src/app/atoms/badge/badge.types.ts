702c09f28634e738156d47d78797ec1ae24f0f85
diegozsr1 <diegozsr1@gmail.com>
2026-06-11 13:27:00 +0200

    Creaccion Bagde Atom Component

---
+++ src/app/atoms/badge/badge.types.ts
@@ -0,0 +1,3 @@
+export type BadgeVariant = 'new' | 'asnew' | 'good' | 'acceptable' | 'pro-primary' | 'pro-secondary' | 'verified' | 'seller-new';
+export type BadgeIcon = 'check' | 'zap' | 'message-circle' | 'none';
+export type BadgeIconPosition = 'left' | 'right';
