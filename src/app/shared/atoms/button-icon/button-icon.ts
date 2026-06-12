69f2797469f37f49600dc697a58e5f7ad360cdaf
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-12 18:15:20 +0200

    feat(button-icon): create button-icon component

---
+++ src/app/shared/atoms/button-icon/button-icon.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'atom-button-icon',
+  imports: [],
+  templateUrl: './button-icon.html',
+  styleUrl: './button-icon.css',
+})
+export class ButtonIcon {
+
+}
