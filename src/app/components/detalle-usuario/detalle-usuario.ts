dfbb83348a7dfcff58b5ead1e620de919ac6ddf2
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-06-13 00:16:31 +0200

    maquetar detalle usuario

--- src/app/components/detalle-usuario/detalle-usuario.ts
+++ src/app/components/detalle-usuario/detalle-usuario.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-detalle-usuario',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './detalle-usuario.html',
   styleUrl: './detalle-usuario.css',
 })
 export class DetalleUsuario {
+  miForm: FormGroup;
+  mensaje: string = '';
+  tipo: boolean = false;
 
+  constructor(private cd: ChangeDetectorRef) {
+    this.miForm = new FormGroup({
+      rol: new FormControl('',[])
+    }, []);
+  }
+
+  loadData() {
+    if (!this.miForm.valid) {
+      this.miForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.miForm.value);
+  }
 }
