4f362457faa343b48b7146e59ecd4528af2b6f9b
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-06-12 11:01:11 +0200

    añadir maquetación buscador

--- src/app/shared/buscador/buscador.ts
+++ src/app/shared/buscador/buscador.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-buscador',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './buscador.html',
   styleUrl: './buscador.css',
 })
 export class Buscador {
+  textoBusqueda = '';
 
+  elementos = [
+    'Angular',
+    'React',
+    'Vue',
+    'Svelte',
+    'SolidJS'
+  ];
+
+  get resultados() {
+    return this.elementos.filter(item =>
+      item.toLowerCase().includes(this.textoBusqueda.toLowerCase())
+    );
+  }
 }
