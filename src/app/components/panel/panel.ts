e0f9266e51e6b21007cd1e5b7cfec30bb2641a71
Marino <marinosanjurjo.1997@gmail.com>
2026-06-14 12:08:37 +0200

    Creación AD-01 y ajuste de vistas

--- src/app/components/panel/panel.ts
+++ src/app/components/panel/panel.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-panel',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './panel.html',
   styleUrl: './panel.css',
 })
