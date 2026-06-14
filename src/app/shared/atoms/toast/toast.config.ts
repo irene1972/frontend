7a2cec19b20cf42eeed59e451f5afb1c366577a4
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-14 21:16:14 +0200

    feat(config): component configuration

--- src/app/shared/atoms/toast/toast.config.ts
+++ src/app/shared/atoms/toast/toast.config.ts
@@ -1,2 +1,34 @@
 /** Component Type Definitions */
 export type ToastVariant = 'success' | 'info' | 'warn' | 'error' | 'trash'
+
+/** Component State */
+export enum ToastState {
+    EXIST   = 'exist',
+    NOT_EXIST = 'notExist'
+}
+
+/** Component Configuration Interface */
+interface ToastConfig {
+  time:    number;
+}
+
+type ToastVariantMap = Record<ToastVariant, ToastConfig>;
+
+export const CONFIG: ToastVariantMap = {
+  success: {
+    time: 3
+  },
+  info:  {
+    time: 4
+  },
+  warn:  {
+    time:5
+  },
+  error:  {
+    time:5
+  },
+  trash:  {
+    time: 4
+  }
+}
+
