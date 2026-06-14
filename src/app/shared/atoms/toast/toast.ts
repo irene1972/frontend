0a7af4410fc5da21ad06638c00c810a2d11a8646
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-14 16:13:57 +0200

    feat(toast): change selector to atom-toast

--- src/app/shared/atoms/toast/toast.ts
+++ src/app/shared/atoms/toast/toast.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-toast',
+  selector: 'atom-toast',
   imports: [],
   templateUrl: './toast.html',
   styleUrl: './toast.css',
