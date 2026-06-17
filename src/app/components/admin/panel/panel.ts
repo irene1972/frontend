5591f9689768978818fba52e6149c783245fd344
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-06-17 20:19:00 +0200

    fix carga datos en panel

--- src/app/components/admin/panel/panel.ts
+++ src/app/components/admin/panel/panel.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { StatisticsService } from '../../../services/statistics-service';
 
@@ -12,10 +12,13 @@ export class Panel {
   statisticsService = inject(StatisticsService);
   estadisticas: any = {};
 
+  constructor(private cd: ChangeDetectorRef) {}
+
   ngOnInit() {
     this.statisticsService.getStatisticsByPeriod('1m').subscribe({
       next: (data) => {
         this.estadisticas = data;
+        this.cd.detectChanges();
         console.log('Estadísticas:', data);
       },
       error: (error) => {
