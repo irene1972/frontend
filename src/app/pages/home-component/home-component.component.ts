555f902ffd06fa6195ac7dba531e81a62aba9c6a
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-06-18 12:49:43 +0200

    implementar vista mobile

--- src/app/pages/home-component/home-component.component.ts
+++ src/app/pages/home-component/home-component.component.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 import { NLInicio } from '../../components/no_logueado/nl-inicio/nl-inicio';
 import { Heading } from '../../components/no_logueado/heading/heading';
 import { HomeBar } from '../../shared/home-bar/home-bar';
+import { Buscador } from '../../shared/buscador/buscador';
 
 @Component({
   selector: 'app-home-component',
-  imports: [NLInicio,Heading,HomeBar],
+  imports: [NLInicio,Heading,HomeBar,Buscador],
   templateUrl: './home-component.component.html',
   styleUrl: './home-component.component.css',
 })
 export class HomeComponentComponent {
   user:any={};
+  textoBusqueda:string='';
+  placeholder:string='Buscar movil, portatil, tablet...';
 
   ngOnInit(){
     const usuarioString = localStorage.getItem('usuarioBuy&Sell');
