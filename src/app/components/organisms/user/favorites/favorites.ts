e7bbf550130b8cd75d22b8a81d16eaf4ae4ee4d9
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-06-19 23:48:40 +0200

    maquetar usuarios favoritos

--- src/app/components/organisms/user/favorites/favorites.ts
+++ src/app/components/organisms/user/favorites/favorites.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IArticle } from '../../../../interfaces/i-article';
 import { IUsuario } from '../../../../interfaces/i-usuario';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-favorites',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './favorites.html',
   styleUrl: './favorites.css',
 })
