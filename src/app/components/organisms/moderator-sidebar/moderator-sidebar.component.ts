52d5b3c192242ae4696fd1a97b3825750e2bc7d7
Marino <marinosanjurjo.1997@gmail.com>
2026-06-19 17:16:58 +0200

    Añadido cerrar sesión en sidebar moderador

--- src/app/components/organisms/moderator-sidebar/moderator-sidebar.component.ts
+++ src/app/components/organisms/moderator-sidebar/moderator-sidebar.component.ts
@@ -1,6 +1,5 @@
-import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
-import { RatingsService } from '../../../services/ratings-service';
 
 @Component({
   selector: 'app-moderator-sidebar',
@@ -9,30 +8,17 @@ import { RatingsService } from '../../../services/ratings-service';
   styleUrl: './moderator-sidebar.component.css',
 })
 export class ModeratorSidebarComponent {
-  mensaje: string = '';
-  tipo: boolean = false;
   user: any = {};
-  ratingsService = inject(RatingsService);
-  ratings: any = {};
-
-  constructor(private cd: ChangeDetectorRef) { }
 
   ngOnInit(){
     const usuarioString = localStorage.getItem('usuarioBuy&Sell');
     if (usuarioString) {
       this.user = JSON.parse(usuarioString);
-      
-      this.ratingsService.getRatingsByUser(this.user.id).subscribe((data) => {
-      if (data.error) {
-        this.mensaje = data.error;
-        return;
-      } else {
-        console.log(data);
-        this.ratings = data;
-        this.cd.detectChanges();
-      }
-    });
-
     }    
+  }
+
+  logout(): void {
+    localStorage.removeItem('usuarioBuy&Sell');
+    window.location.href = '/login';
   }
 }
