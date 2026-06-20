77e35e1e8ba4c17ccd5527eb28f0f77940d49c31
diegozsr1 <diegozsr1@gmail.com>
2026-06-20 17:29:43 +0200

    MaquetacionV2

--- src/app/pages/product-view-component/product-view-component.component.ts
+++ src/app/pages/product-view-component/product-view-component.component.ts
@@ -3,12 +3,12 @@ import { Button } from "../../components/atoms/button/button";
 import { ArticlesService } from '../../services/articles-service';
 import { IArticle } from '../../interfaces/i-article';
 import { Router } from '@angular/router';
-import { DatePipe } from '@angular/common';
 import { Badge } from "../../components/atoms/badge/badge";
+import { TimeAgoPipe } from '../../shared/pipes/time-ago-pipe';
 
 @Component({
   selector: 'app-product-view-component',
-  imports: [Button, DatePipe, Badge],
+  imports: [Button, TimeAgoPipe, Badge],
   templateUrl: './product-view-component.component.html',
   styleUrl: './product-view-component.component.css',
 })
@@ -16,7 +16,7 @@ export class ProductViewComponentComponent {
   productID = input<string>();
   articleService = inject(ArticlesService);
   product = signal<IArticle | null>(null);
-  router = inject(Router);
+  private router = inject(Router);
 
   ngOnInit() { 
     this.loadProduct();
@@ -30,5 +30,15 @@ export class ProductViewComponentComponent {
     } catch (error) {
       this.router.navigate(['/**'])
     }
+  }
+
+  onContactar(event: MouseEvent) {
+    this.router.navigate(['/profile'])
+  }
+  onComprar(event: MouseEvent) {
+    this.router.navigate(['/product/checkout', this.productID()])
+  }
+  onInformar(event: MouseEvent) {
+    this.router.navigate(['/home'])
   }
 }
