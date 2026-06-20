b5de42b5c91fafc4ca759f25333a35e48fa5e8e5
Marino <marinosanjurjo.1997@gmail.com>
2026-06-20 18:56:21 +0200

    fix merge rama MO-02

--- src/app/services/articles-service.ts
+++ src/app/services/articles-service.ts
@@ -7,16 +7,22 @@ import { environment } from '../../environments/environment';
   providedIn: 'root',
 })
 export class ArticlesService {
-  private baseUrl=`${environment.apiUrl}/articulos/`;
-      httpClient=inject(HttpClient);
-    
-     getArticlesByUser(user_id:number):Observable<any>{
-      return this.httpClient.get<any>(this.baseUrl+`usuario/${user_id}/publicados`);
-    }
-    getArticlesRecentlyUploaded():Observable<any>{
-      return this.httpClient.get<any>(this.baseUrl+`recientes`);
-    }
-    getArticlesBestSellers():Observable<any>{
-      return this.httpClient.get<any>(this.baseUrl+`mas-vendidos`);
-    }
+  private baseUrl = `${environment.apiUrl}/articulos/`;
+  httpClient = inject(HttpClient);
+
+  getArticlesByUser(user_id: number): Observable<any> {
+    return this.httpClient.get<any>(this.baseUrl + `usuario/${user_id}/publicados`);
+  }
+
+  getArticlesRecentlyUploaded(): Observable<any> {
+    return this.httpClient.get<any>(this.baseUrl + 'recientes');
+  }
+
+  getArticlesBestSellers(): Observable<any> {
+    return this.httpClient.get<any>(this.baseUrl + 'mas-vendidos');
+  }
+
+  getArticleById(id: number): Observable<any> {
+    return this.httpClient.get<any>(this.baseUrl + id);
+  }
 }
