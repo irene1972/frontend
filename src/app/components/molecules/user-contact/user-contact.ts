d998490a9b3e549bc42c049536d38a931dc7fae6
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-23 15:01:29 +0200

    feat(user-contact): generate user-contact molecule component

---
+++ src/app/components/molecules/user-contact/user-contact.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'molecule-user-contact',
+  imports: [],
+  templateUrl: './user-contact.html',
+  styleUrl: './user-contact.css',
+})
+export class UserContact {
+
+}
