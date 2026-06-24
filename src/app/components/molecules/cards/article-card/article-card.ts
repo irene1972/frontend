26b17cf01160aae3ce53fcaed417a72f6a5bc1ec
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-24 15:12:19 +0200

    feat(article-card): generate component

---
+++ src/app/components/molecules/cards/article-card/article-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-card',
+  imports: [],
+  templateUrl: './article-card.html',
+  styleUrl: './article-card.css',
+})
+export class ArticleCard {
+
+}
