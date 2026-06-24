3e23d7369f7345ad8f6a84e9c6fb2dd803536e22
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-24 08:59:34 +0200

    feat(nav-tab): template nav-tab and component inputs

--- src/app/components/molecules/nav-tab/nav-tab.ts
+++ src/app/components/molecules/nav-tab/nav-tab.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
+import { NavTabPage } from './nav-tab.config';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-nav-tab',
-  imports: [],
+  selector: 'molecule-nav-tab',
+  imports: [RouterLink],
   templateUrl: './nav-tab.html',
   styleUrl: './nav-tab.css',
 })
 export class NavTab {
+  public tabs = input<string[]>() 
+  public click = output<number>();
 
+  protected currentTab = signal<number>(0);
+
+  protected activeTab(index: number) {
+    this.click.emit(index)
+  
+  }
 }
