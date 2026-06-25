44c23e29702dd86ccc5f5242a7d23bc1197bdb9f
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-25 22:12:39 +0200

    feat(seller-profile): implement api services

--- src/app/pages/seller-profile/seller-profile.ts
+++ src/app/pages/seller-profile/seller-profile.ts
@@ -1,39 +1,52 @@
-import { Component, computed, inject, input, signal } from '@angular/core';
+import { Component, computed, effect, inject, input, signal } from '@angular/core';
 import { Sidebar } from '../../components/organisms/sidebar/sidebar';
 import { SidebarVariant } from '../../components/organisms/sidebar/sidebar.config';
 import { Breadcrum } from "../../components/molecules/breadcrum/breadcrum";
-import { UserContact } from "../../components/molecules/user-contact/user-contact";
 import { StatusCard } from "../../components/molecules/cards/status-card/status-card";
 import { NavTab } from '../../components/molecules/nav-tab/nav-tab';
 import { NavTabOptions } from '../../components/molecules/nav-tab/nav-tab.config';
 import { ActivatedRoute, Router } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs/operators';
-import { ArticleCard } from "../../components/molecules/cards/article-card/article-card";
+import { ArticleCard } from '../../components/molecules/cards/article-card/article-card';
+import { UserContact } from '../../components/molecules/user-card/user-contact/user-contact';
+import { UserRatingComment } from "../../components/molecules/user-card/user-rating-comment/user-rating-comment";
+import { UsersService } from '../../services/users-service';
+import { RatingsService } from '../../services/ratings-service';
+import { OrdersService } from '../../services/orders-service';
+import { ArticlesService } from '../../services/articles-service';
 
 @Component({
   selector: 'app-seller-profile',
-  imports: [Sidebar, Breadcrum, UserContact, StatusCard, NavTab, ArticleCard],
+  imports: [Sidebar, Breadcrum, UserContact, StatusCard, NavTab, ArticleCard, UserRatingComment],
   templateUrl: './seller-profile.html',
   styleUrl: './seller-profile.css',
 })
 export class SellerProfile {
   //Input
-  public userID = input<string>();
+  public userID = input<string>("1");
   public readonly QUERYPARAM_ON_SELL: string = "on_sell"
   public readonly QUERYPARAM_RATINGS: string = "ratings"
-
-  private router = inject(Router);
-  private actived_route = inject(ActivatedRoute);
+  protected role = signal<SidebarVariant | null>(null);
+ 
   protected articles = signal<number[]>([1,2,3,4,5,6,7,8,9]);
   
  
   //Servicios
-  public role = input<SidebarVariant>('user');
-  
-  user: any = {};
+  user: any = {}
+  protected name      = signal<string>('0');
+  protected last_name = signal<string>('0');
+  protected published = signal<string>('0');
+  protected selled    = signal<string>('0');
+  protected rating    = signal<string>('0');
+  protected breadcrumb_name    = signal<string>('');
 
-  
+  private router = inject(Router);
+  private actived_route = inject(ActivatedRoute);
+  private userService = inject(UsersService);
+  private articleService = inject(ArticlesService)
+  private ratingService = inject(RatingsService)
+  private orderService = inject(OrdersService)
 
   ngOnInit() { 
 
@@ -42,12 +55,68 @@ export class SellerProfile {
         queryParamsHandling: 'merge'  // conserva otros query params existentes
     });
 
-    
     const usuarioString = localStorage.getItem('usuarioBuy&Sell');
+     console.log(this.user)
     if (usuarioString) {
       this.user = JSON.parse(usuarioString);
+      console.log(this.user)
+      this.role.set('user')
     }    
     this.loadUser();
+
+    this.userService.getUserById(this.userID()).subscribe({
+      next: (data) => {
+        if (data.error) {
+          return;
+        } else {
+          this.name.set(data.nombre);
+          this.last_name.set(data.apellidos);
+          this.breadcrumb_name.set(`${this.name()} ${this.last_name()[0]}.`)
+        }
+      },
+      error: (err) => {
+        console.error(err);
+        
+      }
+    });
+
+    this.ratingService.getRatingsByUser(Number(this.userID())).subscribe({
+        next: (data) => {
+          if (data.error) {
+            return;
+          } else {
+            this.rating.set(String(data.puntuacion_media ?? "0"));
+         }
+        },
+        error: (err) => {
+          console.error(err);
+          
+        }
+    });
+
+    this.articleService.getCountArticlesByUser(Number(this.userID())).subscribe({
+        next: (data) => {
+          if (data.error) {
+            return;
+          } else {
+            this.published.set(String(data.total_publicados ?? "0"));
+         }
+        },
+        error: (err) => {
+          console.error(err);
+          
+        }
+    });
+
+     this.orderService.getSalesByUser(Number(this.userID())).subscribe({
+        next: (data) => {
+          this.selled.set(String(data.total_ventas ?? ""))
+        },
+        error: (err) => {
+          console.error(err);
+
+        }
+    });
   }
 
   logout(): void {
@@ -55,21 +124,17 @@ export class SellerProfile {
     window.location.href = '/login';
   }
 
-  //carga de producto y datos
+ //carga de producto y datos
   async loadUser() {
     const id = this.userID();
     if(!id) return;
   
   }
 
-  protected username = computed(()=>{
-    return "Carlos M.";
-  }) 
-
   protected breadcrumbItems = () => [
     { label: 'Inicio', route: '/home' },
     { label: 'Vendedores', route: `/sellers`},
-    { label: this.username(), route: `/sellers/${this.userID()}`}
+    { label: this.breadcrumb_name(), route: `/sellers/${this.userID()}`}
   ];
 
   protected sellerTabs() {
