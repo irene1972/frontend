4f45e563e7f2922e1e66dbdc62224cac29319f0f
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-27 19:56:12 +0200

    feat(): add function state machine for previous and next steps

--- src/app/pages/product-form-component/product-form-component.component.ts
+++ src/app/pages/product-form-component/product-form-component.component.ts
@@ -1,9 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Button } from "../../components/atoms/button/button";
-import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Router } from '@angular/router';
-import { map } from 'rxjs';
 import { NavStep } from "../../components/organisms/navs/nav-step/nav-step";
+import { NavStepOptions } from '../../components/organisms/navs/nav-step/nav-step.config';
 
 @Component({
   selector: 'app-product-form-component',
@@ -12,6 +11,7 @@ import { NavStep } from "../../components/organisms/navs/nav-step/nav-step";
   styleUrl: './product-form-component.component.css',
 })
 export class ProductFormComponentComponent {
+  public readonly QUERYPARAM_NONE:     string = ""
   public readonly QUERYPARAM_DETAIL:   string = "detail"
   public readonly QUERYPARAM_PRICE:    string = "price"
   public readonly QUERYPARAM_PICTURES: string = "pictures"
@@ -20,13 +20,87 @@ export class ProductFormComponentComponent {
   private router = inject(Router);
   private actived_route = inject(ActivatedRoute);
 
-  ngOnInit() { 
+  protected loadSteps(){
+      const detail: NavStepOptions = {name:"1", label:"DETALLE", query_param:this.QUERYPARAM_DETAIL};
+      const price: NavStepOptions = {name:"2", label:"PRECIO", query_param:this.QUERYPARAM_PRICE};
+      const pictures: NavStepOptions = {name:"3", label:"FOTOS", query_param:this.QUERYPARAM_PICTURES};
+      return [detail, price, pictures];
+  }
+
+  protected currentStep = signal<string>("");
+
+  protected nextStep() {
+
+    const steps = this.actived_route.snapshot.queryParamMap.getAll('step');
+    const last = steps.at(-1)
+    switch (last) {
+      case this.QUERYPARAM_DETAIL:
+        this.addQuerypParamStep(this.QUERYPARAM_PRICE)
+        this.currentStep.set(this.QUERYPARAM_PRICE);
+        break;
+      case this.QUERYPARAM_PRICE:
+        this.addQuerypParamStep(this.QUERYPARAM_PICTURES)
+        this.currentStep.set(this.QUERYPARAM_PICTURES);
+        break;
+      case this.QUERYPARAM_PICTURES:
+        break;
+      default:
+        this.addQuerypParamStep(this.QUERYPARAM_DETAIL)
+        this.currentStep.set(this.QUERYPARAM_DETAIL);
+        // código si no coincide ningún caso
+        break;
+    }
+  }
+
+  protected previousStep(){
+    const steps = this.actived_route.snapshot.queryParamMap.getAll('step');
+    const last = steps.at(-1)
     
+    switch (last) {
+      case this.QUERYPARAM_PICTURES:
+        this.removeQueryParamStep(this.QUERYPARAM_PICTURES)
+        this.currentStep.set(this.QUERYPARAM_PRICE);
+        break;
+      case this.QUERYPARAM_PRICE:
+        this.removeQueryParamStep(this.QUERYPARAM_PRICE)
+        this.currentStep.set(this.QUERYPARAM_DETAIL);
+        break;
+      case this.QUERYPARAM_DETAIL:
+        this.removeQueryParamStep(this.QUERYPARAM_DETAIL)
+        this.currentStep.set(this.QUERYPARAM_NONE);
+        break;
+      default:
+        this.currentStep.set(this.QUERYPARAM_NONE);
+        // código si no coincide ningún caso
+        break;
+    }
   }
 
 
-  protected activeTab = toSignal(
-    this.actived_route.queryParamMap.pipe(
-        map(params => params.get('step'))
-    ));
+  // Añadir un query param nuevo
+  addQuerypParamStep(value: string) {
+    const currentSteps = this.actived_route.snapshot.queryParamMap.getAll('step');
+    
+    // evita duplicados
+    if (currentSteps.includes(value)) return;
+
+    this.router.navigate([], {
+      queryParams: { step: [...currentSteps, value] },
+      queryParamsHandling: 'merge'
+    });
+  }
+
+  removeQueryParamStep(value: string) {
+    const currentSteps = this.actived_route.snapshot.queryParamMap.getAll('step');
+    
+    // elimina el valor del array
+    const newSteps = currentSteps.filter(step => step !== value);
+
+    this.router.navigate([], {
+      queryParams: { step: newSteps },
+      queryParamsHandling: 'merge'
+  });
+   }
+
+
 }
