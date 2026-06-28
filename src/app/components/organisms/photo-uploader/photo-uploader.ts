60ce21464c164c266691bdc0b7e785d0bdeeba0e
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-28 10:28:28 +0200

    feat(photo-uploader): generate component

---
+++ src/app/components/organisms/photo-uploader/photo-uploader.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-uploader',
+  imports: [],
+  templateUrl: './photo-uploader.html',
+  styleUrl: './photo-uploader.css',
+})
+export class PhotoUploader {
+
+}
