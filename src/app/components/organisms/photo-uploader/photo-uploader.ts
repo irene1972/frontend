f7748d215e1549480d855e990f872001b223aedf
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-06-30 14:30:17 +0200

    feat(photo-uploader): add individual buttons to photo previews and change order of images.

--- src/app/components/organisms/photo-uploader/photo-uploader.ts
+++ src/app/components/organisms/photo-uploader/photo-uploader.ts
@@ -1,17 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, model, output, signal } from '@angular/core';
 import { PhotoUploaderEvent as PUEvent } from './photo-uploader.config';
 import { Icon } from "../../atoms/icon/icon";
 import { Button } from "../../atoms/button/button";
+import { Data } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Badge } from "../../atoms/badge/badge";
 
 @Component({
   selector: 'organism-photo-uploader',
-  imports: [Icon, Button],
+  imports: [Icon, Button, Badge],
   templateUrl: './photo-uploader.html',
   styleUrl: './photo-uploader.css',
 })
 export class PhotoUploader {
-  protected dragEvent = signal<PUEvent>(PUEvent.DRAG);
-  protected photos = signal<string[]>(["", "", "", ""]);
+  protected dragEvent     = signal<PUEvent>(PUEvent.DRAG);
+  protected photoPreviews = signal<string[]>(["", "", "", ""]);
+  private   files         = signal<(File | null)[]>([null, null, null, null]); 
+  readonly  photoFiles    = output<(File | null)[]>();
+  
+  
+
   onDragOver(event: DragEvent) {
     event.preventDefault();
     this.dragEvent.set(PUEvent.DRAG_OVER)
@@ -25,23 +33,71 @@ export class PhotoUploader {
   onDrop(event: DragEvent) {
     event.preventDefault();
     const files = event.dataTransfer?.files;
-    if (files) {
-      console.log(Array.from(files));
-    }
+    this.uploadPhotoList(files);
   }
 
-  protected previewCaption = signal<string | null>(null);
-
-  protected uploadPhotos(event:Event) {
+  protected onChange(event:Event){
     const input = event.target as HTMLInputElement;
-    const updfiles = Array.from(input.files ?? []);
-    
-    for(let i=0; i< this.photos().length; i++ ){
-      if(!this.photos()[i]){
-        // Creamos una url para la imagen
-        const url = URL.createObjectURL(updfiles[i]);
-        this.photos.update((current) =>[] );
+    this.uploadPhotoList(input.files) 
+    // Resetea el input para permitir seleccionar el mismo archivo otra vez
+    input.value = '';
+  }
+
+  protected selectFile(event:Event, index:number){
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.addPhoto(input.files[0], index); 
+    }
+     // Resetea el input para permitir seleccionar el mismo archivo otra vez
+    input.value = '';
+  }
+
+
+  protected uploadPhotoList(files: FileList | undefined | null) {
+    if(files){
+      for (let i=0; i<files.length; i++) {
+        if (files.length>this.photoPreviews().length) return;
+        const preview_url = URL.createObjectURL(files[i]);
+
+        this.photoPreviews.update(current => {
+          const freeIndex = current.findIndex(p => !p); // primer slot vacío/null/""
+          if (freeIndex === -1) return current; // no hay sitio libre, no hace nada
+          return current.map((p, i) => i === freeIndex ? preview_url : p);
+        });
+
+        this.files.update(current => {
+          const freeIndex = current.findIndex(p => !p); // primer slot vacío/null/""
+          if (freeIndex === -1) return current; // no hay sitio libre, no hace nada
+          return current.map((p, i) => i === freeIndex ? files[i] : p);
+        });
+
+        this.photoFiles.emit(this.files())
       }  
     }
   }
+
+  protected addPhoto(file: File | undefined | null , index: number) {
+    if(file){
+      const preview_url = URL.createObjectURL(file);
+      this.photoPreviews.update(current => [...current.slice(0, index), preview_url, ...current.slice(index + 1)]);
+      this.files.update(current => [...current.slice(0, index), file, ...current.slice(index + 1)]);
+      this.photoFiles.emit(this.files())
+    }  
+  }
+
+  protected removePhoto(index: number) {
+    URL.revokeObjectURL(this.photoPreviews()[index]); // libera memoria
+    this.photoPreviews.update(photos => {
+      photos[index] = ""; 
+      return [...photos];
+    } );
+
+    this.files.update(files => {
+      files[index] = null; 
+      return [...files];
+    } );
+
+    this.photoFiles.emit(this.files());
+  }
+
 }
