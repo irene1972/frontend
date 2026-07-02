c9afa8cb765e431afaa5cd79a976774495d32e44
Gabo Avalos <gabriel_hzh@hotmail.com>
2026-07-02 11:33:11 -0500

    configuracion para prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api',
-  socketUrl: 'http://localhost:3000',
+  apiUrl: 'https://bk-buy-sell-prod.up.railway.app/api',
+  socketUrl: 'https://bk-buy-sell-prod.up.railway.app',
 };
