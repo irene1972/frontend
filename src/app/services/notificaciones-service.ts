530ccd9b0ecb729af038f51686c56ebe98bea430
Gabo Avalos <gabriel_hzh@hotmail.com>
2026-07-03 16:37:26 -0500

    Update notificaciones-service.ts

--- src/app/services/notificaciones-service.ts
+++ src/app/services/notificaciones-service.ts
@@ -1,28 +1,28 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificacionesService {
   // Esta es la dirección de tu servidor Node.js
-  private apiUrl = 'http://localhost:3000/api/notificaciones'; 
-
-  constructor(private http: HttpClient) { }
+  private baseUrl = `${environment.apiUrl}/notificaciones`;
+  httpClient = inject(HttpClient);
 
   // 1. Pide la lista de notificaciones a tu base de datos de Railway
   getNotificaciones(usuarioId: number): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/usuario/${usuarioId}`);
+    return this.httpClient.get<any[]>(`${this.baseUrl}/usuario/${usuarioId}`);
   }
 
   // 2. Cuenta las notificaciones sin leer para poner el número en la campana
   getContarSinLeer(usuarioId: number): Observable<{ sinLeer: number }> {
-    return this.http.get<{ sinLeer: number }>(`${this.apiUrl}/usuario/${usuarioId}/sin-leer`);
+    return this.httpClient.get<{ sinLeer: number }>(`${this.baseUrl}/usuario/${usuarioId}/sin-leer`);
   }
 
   // 3. Cambia el estado en la base de datos a leídas (leida = 1)
   marcarComoLeidas(usuarioId: number): Observable<any> {
-    return this.http.put(`${this.apiUrl}/usuario/${usuarioId}/leer`, {});
+    return this.httpClient.put(`${this.baseUrl}/usuario/${usuarioId}/leer`, {});
   }
 }
