5be6dc4019a8180d92c002ea5a3cc0adaa8e204d
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-07-03 09:45:55 +0200

    corregir la query que cuenta los usuarios

--- src/app/components/organisms/admin/panel/panel.ts
+++ src/app/components/organisms/admin/panel/panel.ts
@@ -2,6 +2,8 @@ import { ChangeDetectorRef, Component, inject, input } from '@angular/core';
 import { StatisticsService } from '../../../../services/statistics-service';
 import { StatusCard } from "../../../molecules/cards/status-card/status-card";
 import { ManagementCard } from '../../../molecules/cards/management-card/management-card';
+import { UsersService } from '../../../../services/users-service';
+import { IUsuario } from '../../../../interfaces/i-usuario';
 
 @Component({
   selector: 'app-panel',
@@ -12,9 +14,11 @@ import { ManagementCard } from '../../../molecules/cards/management-card/managem
 export class Panel {
   statisticsService = inject(StatisticsService);
   estadisticas: any = {};
-  desktop = input<boolean>(false); 
+  desktop = input<boolean>(false);
+  usuarios: IUsuario[] = [];
+  usersService = inject(UsersService);
 
-  constructor(private cd: ChangeDetectorRef) {}
+  constructor(private cd: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.statisticsService.getStatisticsByPeriod('1m').subscribe({
@@ -25,6 +29,22 @@ export class Panel {
       },
       error: (error) => {
         console.error('Error al cargar estadísticas:', error);
+      }
+    });
+    this.usersService.getAllUsers().subscribe({
+      next: (data) => {
+        if (data.error) {
+          console.log(data.error);
+          return;
+        } else {
+          this.usuarios = data;
+          console.log(this.usuarios);
+          this.cd.detectChanges();
+        }
+      },
+      error: (err) => {
+        console.error(err);
+
       }
     });
   }
